43dfd625ef97a548b7aaecc67c6206605b022084
sh1net <mustashev168@gmail.com>
2023-01-07 13:03:33 +0300

    asd

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Main() {
+  return(
+    <div className='container'>
+
+        <ul className="item a">Тесты</ul>
+        <ul className="item b">История</ul>
+        <ul className="item c">Статистика</ul>
+        <ul className="item d">Выйти</ul>
+
+    </div>
+  )
+}
+
+export default Main
