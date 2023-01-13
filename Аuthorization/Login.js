e5781c949856d0cd3e55b52fe0f22c7ffc6ca986
Ahrisai <sasaslapik40@gmail.com>
2023-01-13 15:42:36 +0300

    А почему так нахуй

--- Аuthorization/Login.js
+++ Аuthorization/Login.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Registration from "./Registration";
+import "./MainAuthorizationStyle.css"
+import logo from "./img/logo.png"
+import AuthorizeButton from "./AuthorizeButton/AuthorizeButton";
+import {BrowserRouter, Switch ,Routes,Route,Link} from "react-router-dom"
+import ReactDOMClient from 'react-dom/client';
+import App from "../App";
 
 function Login(){
     return(
-        <div>log</div>
+        <div className="background">
+            <img className="img--logo" src={logo}/>
+            <div className="logo--title">ARISHTEST</div>
+            <div className="athorization--container">
+           
+            
+            <a  href="#" className="support">Связаться с нами</a>
+            </div>
+           
+        </div>
     )
 }
 
