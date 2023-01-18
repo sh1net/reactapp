9dc58e0553123502ec5358063e6038d20639e700
sh1net <mustashev168@gmail.com>
2023-01-18 18:21:41 +0300

    pzdc

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-import App from './App';
-
-const app=ReactDOMClient.createRoot(document.getElementById("app"))
-app.render(<App/>)
