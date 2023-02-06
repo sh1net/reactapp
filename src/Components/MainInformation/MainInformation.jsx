4e39f0e46187e9a61b4c21f243fe34df6f16692a
Ahrisai <sasaslapik40@gmail.com>
2023-02-06 20:32:58 +0300

    Main готов

--- src/Components/MainInformation/MainInformation.jsx
+++ src/Components/MainInformation/MainInformation.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import   '../../Components/MainInformation/MainInformationStyles.css'
+import mainInf_1 from '../../img/mainInf_1.png'
+import mainInf_2 from '../../img/mainInf_2.png'
+import mainInf_3 from '../../img/mainInf_3.png'
 function MainInformation() {
   return (
     <div className='info__container'>
@@ -12,29 +15,30 @@ function MainInformation() {
 пользователей за наивысшее количество баллов и наименьшее время, 
 затраченное на прохождение теста. Удачи вам в тестировании и 
 саморазвитии </div>
-            <img src="" alt="" className="info__item__img" />
+            <img src={mainInf_1} alt="" className="info__item__img" />
         </div>
         <div className="info__item">
-            <img src="" alt="" className="info__item__img" />
+            <img src={mainInf_2} alt="" className="info__item__img" />
             <div className="info__item__text">     Данный проект позволяет отслежтвать наиболее сложные и легкие 
 вопросы для составления тестов новых уровней(от самых легких, до 
-самых сложных).Всего есть несколько уровней сложности тестов:
-Easy
-Normal
-Madium
-Hard
-Wunderkind
+самых сложных) <br />
+.Всего есть несколько уровней сложности тестов: <br/>
+Easy<br/>
+Normal<br/>
+Madium<br/>
+Hard<br/>
+Wunderkind<br/>
 В зависимости от уровня вы будете получать больше или меньше баллов.</div>
         </div>
         <div className="info__item">
             <div className="info__item__text">     Система рейтинга представляет собой набор баллов каждого поль -
-зователя, на основе которого формируется список участников с наиболь -
+зователя, на основе которого формируется список участников с наиболь 
 шим количеством баллов. Данный рейтинг стимулирует нас делать все 
 большее количество тестов, их разновидности и сложности. Также это 
 может помочь с стимулированием других пользователей проходить тесты
 быстрее для получения еще большего количества балов.
 Также вы можете получить некоторые звания за места в списке лидеров.</div>
-            <img src="" alt="" className="info__item__img" />
+            <img src={mainInf_3} alt="" className="info__item__img" />
         </div>
     </div>
   )
